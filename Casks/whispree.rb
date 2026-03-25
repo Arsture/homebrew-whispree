ec3f7b2bd8e2c6b945b3bb2554f30b4a74d9fd76
Arsture <kms23806512@gmail.com>
2026-03-25 14:16:57 +0900

    chore: bump whispree to 0.0.13

--- Casks/whispree.rb
+++ Casks/whispree.rb
@@ -1,6 +1,6 @@
 cask "whispree" do
-  version "0.0.14"
-  sha256 "905d30e46d2de5c82d72e6a1c9b7375ec8d2a52511653666be8e4dd47d713878"
+  version "0.0.13"
+  sha256 "7ccbd79e0cef82f15a9c439fe32c7d69ff4fc159acf1b697f7550ade16c444bc"
 
   url "https://github.com/Arsture/whispree/releases/download/v#{version}/Whispree.zip"
   name "Whispree"
